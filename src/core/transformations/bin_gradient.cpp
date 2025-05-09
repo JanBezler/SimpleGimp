f34eb6344b8db58650399b86c9b9aa07371648cf
Szarpest <jan.filip.bezler@gmail.com>
2025-05-09 15:13:02 +0200

    progowanie gradient

--- src/core/transformations/bin_gradient.cpp
+++ src/core/transformations/bin_gradient.cpp
@@ -1,4 +1,4 @@
-#include "bin_gradient.h"
+﻿#include "bin_gradient.h"
 
 BinarizationGradient::BinarizationGradient(PNM* img) :
     Transformation(img)
@@ -17,7 +17,29 @@ PNM* BinarizationGradient::transform()
 
     PNM* newImage = new PNM(width, height, QImage::Format_Mono);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    double numerator = 0.0;
+    double denominator = 0.0;
+
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            int gx = 0;
+            int gy = 0;
+
+            if (x > 0 && x < width - 1) gx = qGray(image->pixel(x + 1, y)) - qGray(image->pixel(x - 1, y));
+            if (y > 0 && y < height - 1) gy = qGray(image->pixel(x, y + 1)) - qGray(image->pixel(x, y - 1));
+
+            int gMax = std::max(std::abs(gx), std::abs(gy));
+            numerator += gMax * qGray(image->pixel(x, y));
+            denominator += gMax;
+        }
+    }
+
+    double threshold = (denominator != 0) ? (numerator / denominator) : 0;
+    for (int y = 0; y < height; y++)
+        for (int x = 0; x < width; x++)
+            newImage->setPixel(x, y, qGray(image->pixel(x, y)) > threshold ? Qt::color1 : Qt::color0);
 
     return newImage;
 }
--- src/core/transformations/bin_manual.cpp
+++ src/core/transformations/bin_manual.cpp
@@ -20,7 +20,7 @@ PNM* BinarizationManual::transform()
     PNM* newImage = new PNM(width, height, QImage::Format_Mono);
     for (int x = 0; x < width; x++)
         for (int y = 0; y < height; y++)
-            newImage->setPixel(x, y, qGray(image->pixel(x, y)) >= threshold);
+            newImage->setPixel(x, y, qGray(image->pixel(x, y)) >= threshold ? Qt::color1 : Qt::color0);
         
     return newImage;
 }
