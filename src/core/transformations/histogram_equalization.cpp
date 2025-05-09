d77c40df121391f20d2e60efe8e801d400adf7ad
Szarpest <jan.filip.bezler@gmail.com>
2025-05-09 12:57:06 +0200

    wyrownanie histogramu

--- src/core/transformations/histogram_equalization.cpp
+++ src/core/transformations/histogram_equalization.cpp
@@ -14,12 +14,43 @@ HistogramEqualization::HistogramEqualization(PNM* img, ImageViewer* iv) :
 
 PNM* HistogramEqualization::transform()
 {
-    int width = image->width();
-    int height = image->height();
+    const int width = image->width();
+    const int height = image->height();
+    const int totalPixels = width * height;
 
     PNM* newImage = new PNM(width, height, image->format());
+    Histogram* histogram = image->getHistogram();
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    QVector<int> cdfR(256, 0), cdfG(256, 0), cdfB(256, 0);
+
+    auto calculateCDF = [&](Histogram::Channel channel, QVector<int>& cdf) {
+        QHash<int, int>* channelHistogram = histogram->get(channel);
+        QVector<float> probabilities(256, 0.0f);
+        float cumulative = 0.0f;
+
+        for (int i = 0; i < 256; i++)
+        {
+            probabilities[i] = static_cast<float>(channelHistogram->value(i, 0)) / totalPixels;
+            cumulative += probabilities[i];
+            cdf[i] = static_cast<int>(cumulative * 255);
+        }
+        };
+
+    calculateCDF(Histogram::RChannel, cdfR);
+    calculateCDF(Histogram::GChannel, cdfG);
+    calculateCDF(Histogram::BChannel, cdfB);
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+        {
+            QRgb pixel = image->pixel(x, y);
+            int r = cdfR[qRed(pixel)];
+            int g = cdfG[qGreen(pixel)];
+            int b = cdfB[qBlue(pixel)];
+            newImage->setPixel(x, y, qRgb(r, g, b));
+        }
+    }
 
     return newImage;
 }
