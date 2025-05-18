6b7c470941c518300692aedaf3cfeb3debe248ef
Szarpest <jan.filip.bezler@gmail.com>
2025-05-18 14:34:19 +0200

    edge laplace

--- src/core/transformations/edge_laplacian.cpp
+++ src/core/transformations/edge_laplacian.cpp
@@ -1,4 +1,4 @@
-#include "edge_laplacian.h"
+﻿#include "edge_laplacian.h"
 
 EdgeLaplacian::EdgeLaplacian(PNM* img) :
     Convolution(img)
@@ -10,12 +10,16 @@ EdgeLaplacian::EdgeLaplacian(PNM* img, ImageViewer* iv) :
 {
 }
 
-math::matrix<float> EdgeLaplacian::getMask(int, Mode)
+math::matrix<float> EdgeLaplacian::getMask(int size, Mode)
 {
-    int size = getParameter("size").toInt();
     math::matrix<float> mask(size, size);
 
-    qDebug() << Q_FUNC_INFO << "Not implemented yet!";
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size; j++)
+            mask(i, j) = -1.0f;
+
+    int center = static_cast<int>(size / 2);
+    mask(center, center) = size * size - 1;
 
     return mask;
 }
